dee3e33f36323aadfb18a32f6b4463c4728161f3
Charlie Skibsted <charlieskibsted@gmail.com>
2023-01-29 13:15:58 +0100

    Added the last methods and cleaned up the code

--- Labb 3 Anropa Databasen SQL och ORM/Program.cs
+++ Labb 3 Anropa Databasen SQL och ORM/Program.cs
@@ -8,88 +8,58 @@ namespace Labb_3_Anropa_Databasen_SQL_och_ORM
         static void Main(string[] args)
         {
             RunProgram();
-
-
-            //SchoolDbContext context = new SchoolDbContext();
-
-            // 1:
-            //Hämta alla elever (ska lösas med Entity framwork)
-            //Användaren får välja om de vill se eleverna sorterade på för- eller efternamn
-            //och om det ska vara stigande eller fallande sortering.
-
-            //var allStudents = context.Students
-            //    .Where(p => p.StudentId > 0)
-            //    .OrderBy(p => p.FirstName);
-
-            //foreach (Student item in allStudents)
-            //{
-            //    Console.WriteLine($"Name: {item.FirstName} {item.LastName}");
-            //}
-
-            //Console.ReadLine();
-
-            /* 2 : 
-            - [ ]  Hämta alla elever i en viss klass (ska lösas med Entity framework)
-            Användaren ska först få se en lista med alla klasser som finns, 
-            sedan får användaren välja en av klasserna och då skrivs alla elever i den klassen ut.
-            Extra utmaning: låt användaren även få välja sortering på eleverna som i punkten ovan.
-            */
-
-            
-
-
-            /*
-             - [ ]  Lägga till ny personal (ska lösas genom Entity framework)
-            Användaren får möjlighet att mata in uppgifter om en ny anställd och den datan sparas då ner i databasen.
-            */
-            //Exempel
-            //Staff S1 = new Staff()
-            //{
-            //    FirstName = "Majje",
-            //    LastName = "Karlsson",
-            //    PersonalId = "921102-4511",
-            //    Adress = "Eftra pl517",
-            //    City = "Slange",
-            //    Phone = "0708491827",
-            //    Email = "Maj@gmail.com",
-
-            //};
-
-            //context.Staff.Add(S1);
-
-            //context.SaveChanges();
-
         }
 
         public static void RunProgram()
         {
             RunMenu();
-
         }
 
         public static void RunMenu()
         {
-            Console.WriteLine("Choose an option:");
-            Console.WriteLine("1. List all students");
-            Console.WriteLine("2. List all students of a certain class");
-            Console.WriteLine("3. Add new staff");
+            bool isLooping = true;
 
-            int choice = Convert.ToInt32(Console.ReadLine());
-
-            switch (choice)
+            while (isLooping)
             {
-                case 1:
-                    GetAllStudents();
-                    break;
-                case 2:
-                    GetStudentsOfClass();
-                    break;
-                case 3:
-                    Console.WriteLine("You chose Option 3");
-                    break;
-                default:
-                    Console.WriteLine("Invalid option. Please try again.");
-                    break;
+                Console.Clear();
+
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. List all students");
+                Console.WriteLine("2. List all students of a certain class");
+                Console.WriteLine("3. Add new staff");
+                Console.WriteLine("4. View all staffmembers");
+                Console.WriteLine("5. Exit program");
+
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        GetAllStudents();
+                        PressAnyKeyToContinue();
+                        break;
+                    case 2:
+                        GetStudentsOfClass();
+                        PressAnyKeyToContinue();
+                        break;
+                    case 3:
+                        AddNewStaff();
+                        PressAnyKeyToContinue();
+                        break;
+                    case 4:
+                        GetAllStaff();
+                        PressAnyKeyToContinue();
+                        break;
+
+                    case 5:
+                        isLooping = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option. Please try again.");
+                        break;
+                }
+
             }
         }
 
@@ -148,7 +118,8 @@ namespace Labb_3_Anropa_Databasen_SQL_och_ORM
             }
             foreach (Student item in allStudents)
             {
-                Console.WriteLine($"Name: {item.FirstName} {item.LastName}");
+                Console.WriteLine($"{item.StudentId}. {item.FirstName} {item.LastName} | PersonalId: {item.PersonalId}" +
+                    $" | Adress: {item.Adress} | City: {item.City} | Phone: {item.City} | Email: {item.Email}");
             }
 
         }
@@ -156,22 +127,6 @@ namespace Labb_3_Anropa_Databasen_SQL_och_ORM
         public static void GetStudentsOfClass()
         {
             SchoolDbContext context = new SchoolDbContext();
-
-
-            //var allClasses = context.Students
-            //    .Where(p => p.StudentId > 0)
-            //    .OrderBy(p => p.Class);
-
-            //Student lastItem = new Student();
-
-            //foreach (Student item in allClasses)
-            //{
-            //    if (lastItem.Class != item.Class)
-            //    {
-            //        lastItem = item;
-            //        Console.WriteLine($"classes: {lastItem.Class} ");
-            //    }
-            //}
 
             Console.WriteLine("Sort by first name (1) or last name (2)?");
             int sortBy = Convert.ToInt32(Console.ReadLine());
@@ -185,6 +140,7 @@ namespace Labb_3_Anropa_Databasen_SQL_och_ORM
 
             Student lastItem = new Student();
 
+            //Skriver ut all existerande klasser på skolan
             Console.WriteLine("Existing Classes:");
             foreach (Student item in allClasses)
             {
@@ -246,6 +202,66 @@ namespace Labb_3_Anropa_Databasen_SQL_och_ORM
             }
 
 
+        }
+
+        public static void AddNewStaff()
+        {
+            SchoolDbContext context = new SchoolDbContext();
+
+            Staff newStaff = new Staff()
+            {
+                FirstName = "",
+                LastName = "",
+                PersonalId = "",
+                Adress = "",
+                City = "",
+                Phone = "",
+                Email = "",
+                Position = "",
+            };
+
+            Console.WriteLine("Enter first name: ");
+            newStaff.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter last name: ");
+            newStaff.LastName = Console.ReadLine();
+            Console.WriteLine("Enter personal ID: ");
+            newStaff.PersonalId = Console.ReadLine();
+            Console.WriteLine("Enter address: ");
+            newStaff.Adress = Console.ReadLine();
+            Console.WriteLine("Enter city: ");
+            newStaff.City = Console.ReadLine();
+            Console.WriteLine("Enter phone: ");
+            newStaff.Phone = Console.ReadLine();
+            Console.WriteLine("Enter email: ");
+            newStaff.Email = Console.ReadLine();
+
+            context.Staff.Add(newStaff);
+            context.SaveChanges();
+            Console.WriteLine("New staff member added successfully.");
+
+        }
+
+        public static void GetAllStaff()
+        {
+            SchoolDbContext context = new SchoolDbContext();
+
+            var allStaff = context.Staff
+                .Where(p => p.StaffId > 0);
+
+            foreach (Staff item in allStaff)
+            {
+                Console.WriteLine($"{item.StaffId}. {item.FirstName} {item.LastName} | PersonalId: {item.PersonalId}" +
+                    $" | Adress: {item.Adress} | City: {item.City} | Phone: {item.City} | Email: {item.Email} | Position: {item.Position}");
+            }
+
+        }
+
+        public static void PressAnyKeyToContinue()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            
+            Console.ReadLine();
         }
 
 
